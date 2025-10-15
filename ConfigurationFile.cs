c7e7ac9d1a5dcc3aff3a5390a31668f8e1a7747f
Turbero <dicon18@gmail.com>
2025-10-15 17:20:22 +0200

    Initial release

---
+++ ConfigurationFile.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx.Configuration;
+using BepInEx;
+using System;
+using System.IO;
+using ServerSync;
+
+namespace PvPBiomeDominions
+{
+    internal class ConfigurationFile
+    {
+        private static ConfigEntry<bool> _serverConfigLocked = null;
+
+        public static ConfigEntry<bool> debug;
+        
+
+        private static ConfigFile configFile;
+        private static readonly string ConfigFileName = PvPBiomeDominions.GUID + ".cfg";
+        private static readonly string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        private static readonly ConfigSync ConfigSync = new ConfigSync(PvPBiomeDominions.GUID)
+        {
+            DisplayName = PvPBiomeDominions.NAME,
+            CurrentVersion = PvPBiomeDominions.VERSION,
+            MinimumRequiredVersion = PvPBiomeDominions.VERSION
+        };
+
+        internal static void LoadConfig(BaseUnityPlugin plugin)
+        {
+            {
+                configFile = plugin.Config;
+
+                _serverConfigLocked = config("1 - General", "Lock Configuration", true, "If on, the configuration is locked and can be changed by server admins only.");
+                _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+                debug = config("1 - General", "DebugMode", false, "Enabling/Disabling the debugging in the console (default = false)", false);
+                
+                SetupWatcher();
+            }
+        }
+
+        private static void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private static void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                Logger.Log("Attempting to reload configuration...");
+                configFile.Reload();
+                SettingsChanged(null, null);
+            }
+            catch
+            {
+                Logger.LogError($"There was an issue loading {ConfigFileName}");
+            }
+        }
+
+        private static void SettingsChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private static ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new ConfigDescription(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = configFile.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+    }
+}
---
+++ PvPBiomeDominions.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace PvPBiomeDominions
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class PvPBiomeDominions : BaseUnityPlugin
+    {
+        public const string GUID = "Turbero.PvPBiomeDominions";
+        public const string NAME = "PvP Biome Dominions";
+        public const string VERSION = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        void Awake()
+        {
+            ConfigurationFile.LoadConfig(this);
+
+            harmony.PatchAll();
+        }
+
+        void onDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
