81f0f8a7e656de76c88cbfa3fcd3e4de0ef90a19
Turbero <dicon18@gmail.com>
2025-10-28 16:10:19 +0100

    Adding patch to color map player pins in blue (pve areas) or red (pvp areas)

--- PvPManagement/PvPPatch.cs
+++ PvPManagement/PvPPatch.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 using PvPBiomeDominions.Helpers;
 using PvPBiomeDominions.Helpers.WardIsLove;
@@ -67,8 +68,6 @@ namespace PvPBiomeDominions.PvPManagement
             if (!GameManager.isWackyEpicMMOSystemInstalled())
                 return true;
             
-            Logger.Log("WackyEpicMMOSystem IS installed. Checking levels...");
-            
             if (__instance  != null && __instance.GetType() == typeof(Player))
             {
                 Character attacker = hit.GetAttacker();
@@ -97,4 +96,39 @@ namespace PvPBiomeDominions.PvPManagement
             return true;
         }
     }
+    
+    [HarmonyPatch(typeof(Minimap), "UpdatePlayerPins")]
+    public static class Minimap_UpdatePlayerPins_Patch
+    {
+        static void Postfix(Minimap __instance)
+        {
+            Dictionary<string, ZNet.PlayerInfo> znetPlayerInfos = ZNet.instance.GetPlayerList()
+                .GroupBy(p => p.m_name).ToDictionary(p => p.Key, p => p.First());
+
+            List<Minimap.PinData> m_pins = (List<Minimap.PinData>) GameManager.GetPrivateValue(__instance, "m_pins");
+            foreach (var pin in m_pins)
+            {
+                if (pin.m_type != Minimap.PinType.Player || pin.m_name == Player.m_localPlayer.GetPlayerName())
+                    continue;
+
+                // Get PlayerInfo
+                ZDOID charID = znetPlayerInfos.GetValueSafe(pin.m_name).m_characterID;
+
+                // Find PvP status
+                bool isPVP = false;
+                ZDO zdo = ZDOMan.instance.GetZDO(charID);
+                if (zdo != null)
+                    isPVP = zdo.GetBool("pvp");
+
+                // Update sprint accordingly
+                var img = pin.m_uiElement?.GetComponent<Image>();
+                if (img == null)
+                    continue;
+
+                img.sprite = isPVP ? ImageManager.spriteIconVanillaImage : ImageManager.spriteBlueIconImage;
+            }
+        }
+    }
+
+
 }
