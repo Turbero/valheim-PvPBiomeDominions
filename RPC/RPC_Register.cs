c8fae0b23568da6643b1983950f177b586c0727f
Turbero <dicon18@gmail.com>
2026-04-26 03:29:20 +0200

    Moving RPC tombstone call

--- RPC/RPC_Register.cs
+++ RPC/RPC_Register.cs
@@ -2,14 +2,13 @@ using System;
 using HarmonyLib;
 using PvPBiomeDominions.PositionManagement.UI;
 using PvPBiomeDominions.PvPManagement;
-using PvPBiomeDominions.TombstoneManagement;
 
 namespace PvPBiomeDominions.RPC
 {
     [HarmonyPatch(typeof(Game), "Start")]
     public class GameStartPatch {
         private static void Prefix() {
-            ZRoutedRpc.instance.Register("RPC_TombStoneAlertPlayer", new Action<long, bool>(TombStoneAlertsPatch.RPC_TombStoneAlertPlayer));
+            ZRoutedRpc.instance.Register("RPC_TombStoneAlertPlayer", new Action<long, bool>(RPC_TombstoneAlertPlayer.RPC_TombStoneAlertPlayer));
             ZRoutedRpc.instance.Register("RPC_RequestPlayerRelevantInfo", RPC_PlayersListPanel.RPC_RequestPlayerRelevantInfo);
             ZRoutedRpc.instance.Register("RPC_ResponsePlayerRelevantInfo", new Action<long, ZPackage>(RPC_PlayersListPanel.RPC_ResponsePlayerRelevantInfo));
             ZRoutedRpc.instance.Register("RPC_AddKillToKiller", new Action<long, string>(PvPKillCountPatch.RPC_AddKillToKiller));
